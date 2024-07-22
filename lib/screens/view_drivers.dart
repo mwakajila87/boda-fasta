93fb0bb0bd5de670e7519dde59307c6567e0e6ab
Juma Kassim <jumakassim89@gmail.com>
2024-07-23 02:45:55 +0300

    second commit

---
+++ lib/screens/view_drivers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewDrivers extends StatefulWidget {
+  const ViewDrivers({super.key});
+
+  @override
+  State<ViewDrivers> createState() => _ViewDriversState();
+}
+
+class _ViewDriversState extends State<ViewDrivers> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
